073f194b9ee658ac3b1e333066e788a5949099b2
Adii-tii <sableaditi8@gmail.com>
2026-01-21 15:34:34 +0530

    logical operators

---
+++ examples/example6.js
@@ -0,0 +1,17 @@
+// logical operators
+
+let a = 10;
+let b = 20;
+
+if(a == b || a < b) {
+    console.log("Either a is equal to b or a is less than b");
+}   else {      
+    console.log("a is greater than b");
+}
+
+let x = 2;
+let y = 2;
+
+if(x === y && x > 1) {  
+    console.log("x is strictly equal to y and x is greater than 1");
+}
